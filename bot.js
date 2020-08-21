c496432a2710fd2026cebb864dff0e581847de18
BKenzie7 <46734500+BKenzie7@users.noreply.github.com>
2020-08-21 16:50:23 -0500

    Update

---
+++ bot.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+//
+const TOKEN = process.env.TOKEN;
+const SQLUSERNAME = process.env.SQLUSERNAME;
+const SQLPASSWORD= process.env.SQLPASSWORD;
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+  /*
+  var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: (SQLUSERNAME),
+  password: (SQLPASSWORD),
+  database: "pedodb"
+});
+*/
+bot.on('message', msg => {
+  guildID = msg.guild.id
+  msg.guild.members.fetch()
+  memberIDs = msg.guild.members.cache.keyArray()
+  console.info(memberIDs)
+
+  // confirm ages
+    age = ["i", "i'm", "i am", "i am", "i am", "i am"];
+    minornumber = ["10", "11", "12", "13", "14", "15"]
+    adultnumber = ["21"];
+    youngadultnumber = ["16", "17", "18", "19"];
+
+  for (var i = 0; i < age.length; i++) {
+  if (msg.content.toLowerCase().includes(age[i]) && msg.content.includes(minornumber[i])) {
+    msg.channel.send(`Age confirmed: Minor \n ID: ${msg.author.id}`);
+    break;
+  }
+}
+for (var i = 0; i < age.length; i++) {
+if (msg.content.toLowerCase().includes(age[i]) && msg.content.includes(adultnumber[i])) {
+  msg.channel.send(`Age confirmed: Adult \n ID: ${msg.author.id}`);
+  break;
+}
+}
+for (var i = 0; i < age.length; i++) {
+if (msg.content.toLowerCase().includes(age[i]) && msg.content.includes(youngadultnumber[i])) {
+  msg.channel.send(`Age confirmed: Young Adult \n ID: ${msg.author.id}`);
+  break;
+}
+}
+
+});
+});
+bot.login(TOKEN);
