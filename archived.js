403fa54a9c8da982eee044d1a6e3144d91adeaa3
BKenzie7 <46734500+BKenzie7@users.noreply.github.com>
2020-08-22 13:52:19 -0500

    Update and rename index.js to archived.js

--- archived.js
+++ archived.js
@@ -5,14 +5,6 @@ const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
-  var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "fuckpedos",
-  password: "password",
-  database: "pedodb"
-});
 });
 age = ["i am 13", "i am 14", "i am 15", "i am 16", "i am 17"]
 bot.on('message', msg => {
