f27c12200fc564c1f2c99e7560879d194d58c25d
William Kenzie <williamkenzie81@gmail.com>
2020-10-29 19:18:36 -0500

    Prep for AI
    Notice: Sophie's End of Life is To Be Determined - the project may be terminated before debute.

--- discord.js
+++ discord.js
@@ -9,15 +9,15 @@
    const Discord = require("discord.js");
    const bot = new Discord.Client();
    const mysql = require("mysql");
-   const fetch = require('node-fetch');
-   const fs = require('fs')
+   const fetch = require("node-fetch");
+   const fs = require("fs");
    /* #########################
-		 #  END OFF REQUIREMENTS #
-		 ######################### */
+			#  END OFF REQUIREMENTS #
+			######################### */
    
    /* ###################
-		 #  CONFIGURATION  #
-		 ################### */
+			#  CONFIGURATION  #
+			################### */
    
    // Change these values in the .env file for security
    const DiscordToken = process.env.DiscordToken;
@@ -26,8 +26,8 @@
    const SQLUser = process.env.SQLUser;
    const SQLDatabase = process.env.SQLDatabase;
    /* #########################
-		 #  END OF CONFIGURATION #
-		 ######################### */
+			#  END OF CONFIGURATION #
+			######################### */
    
    // Start Connecting to MySQL and Load the Explicit Reg
    const db = mysql.createConnection({
@@ -38,18 +38,23 @@
 	 database: SQLDatabase
    });
    
+   function update(username, age, points) {
+	 const sql = `INSERT INTO users (Username, Age, Points) VALUES('${username}', '${age}', '${points}) ON DUPLICATE KEY UPDATE Flag='${age}', '${points}`;
+	 db.query(sql, function(err) {
+	   if (err) throw err;
+	   console.log(
+		 `Database Updated \n '${db.query(
+		   `SELECT Username, Age, Points FROM users WHERE Username = ${username}`
+		 )}'`
+	   );
+	 });
+   }
+   
    bot.on("ready", () => {
 	 db.connect(function(err) {
 	   if (err) throw err;
 	 });
-	 bot.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./discord').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./discord/${file}`);
-	bot.returns.set(command.name, command);
-}
+   
 	 console.log("SOPHIE is active!");
    
 	 bot.on("guildMemberAdd", member => {
@@ -68,6 +73,20 @@ for (const file of commandFiles) {
 		   }
 		 }
 	   });
+	   msg.member({
+		embed: {
+		  color: "7289da",
+		  author: {
+			name: bot.user.username
+		  },
+		  title: "Privacy",
+		  description: `Hey there! This server has Sophie installed in it - this means Sophie will be logging any suspicious actvity. If you don't say anything explicit, then it's nothing to worry about - we will always notifiy you when we see suspicious activity and when your messages are logged.`,
+		  timestamp: new Date(),
+		  footer: {
+			text: `https://thesophieproject.github.com/privacy has more information - by continuing in this server, you agree to these terms.`
+		  }
+		}
+	  })
 	 });
    
 	 bot.on("guildCreate", guild => {
@@ -90,29 +109,80 @@ for (const file of commandFiles) {
 	 });
    
 	 bot.on("message", msg => {
-
-		const body = {
-			text: msg.content,
-			message_id: msg.id
-		}
-
-		fetch("http://localhost:5005/model/parse", {
-			method: "post",
-			body: JSON.stringify(body),
-			headers: { "Content-Type": "application/json" }
-		  })
-		  .then(res => res.json()) //res.json()
-		  .then(json => {
-			  const result = json
-			  console.log(result.intent)
-			  console.log(result.intent.name)
-			  console.log(result.intent.confidence)
-			  bot.commands.get(returns).execute(result.intent.name, result.intent.confidence);
-			})
-		  .catch(err => console.log(err))
-
-		  
-		});
+	   // These are all the functions that make the code work
+   
+	   function AGE(confidence, age) {
+		 msg.author
+		   .send({
+			 embed: {
+			   color: "7289da",
+			   author: {
+				 name: bot.user.username
+			   },
+			   title: "Age Verification",
+			   description: `Hello, ${msg.author.username}! I think you were trying to set your age! Just to confirm, you are ${age}, right?`,
+			   timestamp: new Date(),
+			   footer: {
+				 text: `Just for the record, I was ${confidence}% sure you were trying to get your age.`
+			   }
+			 }
+		   })
+		   .then(msg => {
+			 msg.react("👍").then(() => msg.react("👎"));
+   
+			 const filter = (reaction, user) => {
+			   return (
+				 ["👍", "👎"].includes(reaction.emoji.name) &&
+				 user.id === msg.author.id
+			   );
+			 };
+   
+			 message
+			   .awaitReactions(filter, {
+				 max: 1,
+				 time: 600000000,
+				 errors: ["time"]
+			   })
+			   .then(collected => {
+				 const reaction = collected.first();
+   
+				 if (reaction.emoji.name === "👍") {
+				   // Yes, they are the age - update database
+				 }
+			   })
+			   .catch(collected => {
+				 // Ignore
+			   });
+		   });
+	   }
+   
+	   // ---
+   
+	   const body = {
+		 text: msg.content,
+		 message_id: msg.id
+	   };
+   
+	   fetch("http://localhost:5005/model/parse", {
+		 method: "post",
+		 body: JSON.stringify(body),
+		 headers: { "Content-Type": "application/json" }
+	   })
+		 .then(res => {
+		   res.json(); //res.json()
+		 })
+		 .then(json => {
+		   const result = json;
+		   console.log(result.intent);
+		   console.log(result.intent.name);
+		   console.log(result.intent.confidence);
+		   window[result.intent.name](
+			 result.intent.confidence,
+			 result.entities.age
+		   );
+		 })
+		 .catch(err => console.log(err));
+	 });
    });
    
    bot.login(DiscordToken);
