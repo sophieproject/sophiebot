f31b5188c4bd86d21f27890405e26dc9de907c72
faultydev <faultydev@gmail.com>
2020-11-20 18:34:51 +0100

    bat script..

--- start.bat
+++ start.bat
@@ -1,2 +1,29 @@
-node discord.js
+echo off
+cls
+
+title "The Sophie Project."
+
+echo "";
+echo "████████ ██   ██ ███████     ███████  ██████  ██████  ██   ██ ██ ███████     ██████  ██████   ██████       ██ ███████  ██████ ████████ ";
+echo "   ██    ██   ██ ██          ██      ██    ██ ██   ██ ██   ██ ██ ██          ██   ██ ██   ██ ██    ██      ██ ██      ██         ██    ";
+echo "   ██    ███████ █████       ███████ ██    ██ ██████  ███████ ██ █████       ██████  ██████  ██    ██      ██ █████   ██         ██    ";
+echo "   ██    ██   ██ ██               ██ ██    ██ ██      ██   ██ ██ ██          ██      ██   ██ ██    ██ ██   ██ ██      ██         ██    ";
+echo "   ██    ██   ██ ███████     ███████  ██████  ██      ██   ██ ██ ███████     ██      ██   ██  ██████   █████  ███████  ██████    ██    ";
+echo "";
+echo "Created by FaultyDev and WillKenzie";
+echo "Making the digital world safer.";
+
+timeout /t 3
+
+IF EXIST .\node_modules\ (
+echo "node_modules exists starting npm."
+npm start
+) ELSE (
+echo "node_modules doesn't exist."
+echo "installing node_modules"
+start "npm install"
+start "npm audit fix"
+start "npm start"
+)
+
 pause
