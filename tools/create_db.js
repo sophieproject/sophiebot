3c2d7901ed2daaf04ac2b1a073c17baefc4dd81f
William <46734500+WillKenzie@users.noreply.github.com>
2020-12-08 21:59:03 -0600

    Modular Update / Move to SQLite

---
+++ tools/create_db.js
@@ -0,0 +1,10 @@
+const sqlite3 = require("sqlite3").verbose();
+
+let db = new sqlite3.Database(`./data/sophie.db`, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        core.log(err.message);
+    } else {
+        db.run("CREATE TABLE `users` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT, `Username` LONGTEXT NOT NULL, `Age` INTEGER(3) NOT NULL, `Points` INTEGER(3) NOT NULL, `Admin` INTEGER(1) NOT NULL DEFAULT '0', `Pedophile` INTEGER(1) NOT NULL DEFAULT '0', `Suspicious` INTEGER(1) NOT NULL DEFAULT '0', `Modified` TIMESTAMP NOT NULL);")
+        db.run("CREATE TABLE `messages` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT, `Message` LONGTEXT NOT NULL, `Sender` INTEGER NOT NULL);")
+    }
+});
