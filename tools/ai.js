ab4f87672fd7d76aedb14432457ccc3725d69329
William <46734500+WillKenzie@users.noreply.github.com>
2020-12-14 12:38:28 -0600

    Update ai.js

--- tools/ai.js
+++ tools/ai.js
@@ -1,10 +1,22 @@
 const { dockStart } = require('@nlpjs/basic');
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
 
 (async () => {
   const dock = await dockStart({ use: ['Basic']});
   const nlp = dock.get('nlp');
   await nlp.addCorpus('./models/en-US.json');
   await nlp.train();
-  const response = await nlp.process('en', 'Who are you');
-  console.log(response);
+  while (true) {
+  rl.question("Message> ", async function(message) {
+  const response = await nlp.process('en', message);
+  console.log(response.intent + "\n" + response.score);
+  });
+}
+  
 })();
