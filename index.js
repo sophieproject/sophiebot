d7a4f835e5800e97953ed3474f1b03a3aee1075e
William <46734500+WillKenzie@users.noreply.github.com>
2020-12-23 04:34:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ or otherwise.
 Sophiebot is protected under the MIT License
 
 (c) MIT License by the Sophie Foundation
+
+We have compressed Sophie into multiple different
+files, one of which is core.js
+which is designed to make updates to Sophie
+as universal and painless as possible
+
+index.js is used to load the needed files and keep
+them loaded should a crash start, to minimize downtime
+
+any files in /bots are to be loaded below
+
+training the AI here so it doesn't have to be done
+on every crash to minimize downtime
 */
-
-// We have compressed Sophie into multiple different
-// files, one of which is core.js
-// which is designed to make updates to Sophie
-// as universal and painless as possible
-
-// index.js is used to load the needed files and keep
-// them loaded should a crash start, to minimize downtime
-
-// any files in /bots are to be loaded below
-
-// training the AI here so it doesn't have to be done
-// on every crash to minimize downtime
 
 
 async function Init() {
